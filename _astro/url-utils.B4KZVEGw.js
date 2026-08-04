f9c7aaf267237f0288959f28f61815dc6a8aea9b
xyh2274 <45124830+xyh2274@users.noreply.github.com>
2026-08-04 09:48:34 +0000

    Deploying to gh-pages from @ xyh2274/my-astro-site@953c391436ccc52a79f830dfa7922d850b04ec8f 🚀

--- _astro/url-utils.B4KZVEGw.js
+++
@@ -1,1 +0,0 @@
-import"./translation.jmcKECFg.js";import"./config.CiiPjpci.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
